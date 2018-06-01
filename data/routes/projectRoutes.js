17b3629cbb465c0cc74df4871efce1485a5e9dcd
mister-corn <code@ronaldlibago.com>
2018-05-31 20:13:43 -0700

    T+1hr12min: Project endpoints established

--- data/routes/projectRoutes.js
+++ data/routes/projectRoutes.js
@@ -7,8 +7,28 @@ const projectModel = require('../helpers/projectModel.js');
 
 /* --- '/api/projects' Endpoints --- */
 // Create
+router.post('/', (req, res) => {
+  const { name, description } = req.body;
+  let { completed } = req.body;
+  // if (!name || !description) {
+  //   console.log("'/api/projects' Possible incomplete request:",req.body);
+  //   res.status(400).json({error:"Please ensure name and description is in body of the request."});
+  //   return;
+  // }
+  // if (!completed) completed === false;
+  //==>
+  projectModel.insert({ name, description, completed })
+    .then(project =>{
+      res.json(project);
+    })
+    .catch(err => {
+      console.log(`'/api/projects' POST error:`,err);
+      res.status(500).json({error: 'Project could not be added.'});
+    });
+});
 
 // Read
+// // Retrieve all projects
 router.get('/', (req, res) => {
   //==>
   projectModel.get()
@@ -19,9 +39,60 @@ router.get('/', (req, res) => {
       console.log(`'/api/projects' GET error:`,err);
       res.status(500).json({error: 'Projects could not be retrieved.'});
     })
-})
+});
+// // Retrieve a specific project
+router.get('/:projectId', (req, res) => {
+  const { projectId } = req.params;
+  //==>
+  projectModel.get(projectId)
+    .then(project =>{
+      if (project.length === 0) {
+        res.status(404).json({error:`Project with ID:${projectId} not found.`})
+      }
+      res.json(project);
+    })
+    .catch(err => {
+      console.log(`'/api/projects/${projectId}' GET error:`,err);
+      res.status(500).json({error: 'Projects could not be retrieved.'});
+    });
+});
+
 // Update
+router.put('/:projectId', (req, res) => {
+  const { projectId } = req.params;
+  const project = req.body;
+  //==>
+  projectModel.update(projectId, project)
+    .then(project =>{
+      if (project === null) {
+        res.status(404).json({error:`Project with ID:${projectId} not found.`})
+      }
+      res.json(project);
+    })
+    .catch(err => {
+      console.log(`'/api/projects' POST error:`,err);
+      res.status(500).json({error: 'Projects could not be added.'});
+    });
+});
 
 // Delete
+router.delete('/:projectId', (req, res) => {
+  const { projectId } = req.params;
+  //==>
+  projectModel.remove(projectId)
+    .then(count => {
+      console.log("'/api/projects' DELETE count:",count);
+      if (count === 0) {
+        res.status(500).json({error:`Project with ID: ${projectId} could not be deleted.`});
+        return;
+      } else {
+        res.json({message:"Deletion Successful."});
+      }
+    })
+    .catch(err => {
+      console.log(`'/api/projects' DELETE error:`,err);
+      res.status(500).json({error: 'Projects could not be deleted.'});
+    });
+});
 
 module.exports = router;
